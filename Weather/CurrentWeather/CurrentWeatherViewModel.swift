597d4f808559e9b4d5470a4fdf7ddc06025455fc
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2024-05-17 10:49:48 +0800

    Init files for API

---
+++ Weather/CurrentWeather/CurrentWeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentWeatherViewModel.swift
+//  Weather
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 17/5/2024.
+//
+
+import Foundation
+
+
+class CurrentWeatherViewModel {
+    
+}
